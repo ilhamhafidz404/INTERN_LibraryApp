a5a22bc167ddc9b6dd33b69bc3a12252d51ce4e4
ilhamhafidz404 <xxspanzie@egmail.com>
2025-07-01 15:50:01 +0700

    Bottom Navigation

---
+++ lib/main_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:intern_libraryapp/pages/home_page.dart';
+import 'package:intern_libraryapp/pages/profile_page.dart';
+import 'package:intern_libraryapp/pages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [HomePage(), SearchPage(), ProfilePage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
